601ba527d67ce2983f2d94470254f1323062be67
liushuo <tide_0829@qq.com>
2024-12-09 23:42:37 +0800

    ls测试

---
+++ tmpCode/42decbad-5671-4935-be96-89b16a53ab1c/Main.java
@@ -0,0 +1,28 @@
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) {
+      //请在此处编写代码
+      Scanner scanner = new Scanner(System.in);  
+        String input = scanner.nextLine();  
+        scanner.close();  
+        if (isUnique(input)) {  
+        System.out.println("YES");  
+    } else {  
+        System.out.println("NO");  
+    }  
+}  
+
+public static boolean isUnique(String input) {  
+    input = input.toLowerCase();  
+    HashSet<Character> charHashSet = new HashSet<>();  
+
+    for (int i = 0; i < input.length(); i++) {  
+        if (!charHashSet.add(input.charAt(i))) {  
+            return false;  
+        }  
+    }  
+    
+    return true;  
+    }
+}
---
+++ tmpCode/4b1aac32-ed28-4f63-962c-12be7b5b0919/Main.java
@@ -0,0 +1,7 @@
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) {
+      //请在此处编写代码
+    }
+}
